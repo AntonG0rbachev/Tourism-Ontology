2b510c479e02f50285f5adbb2c75d2733e62a471
DocLivsey <doclivsey@github.com>
2024-12-18 13:21:27 +0300

    feat: made main window init

--- application/app.py
+++ application/app.py
@@ -1,3 +1,14 @@
 import sys
 
-from PyQt5.
+from PyQt5.QtWidgets import *
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(762, 564)
+    w.move(200, 100)
+    w.setWindowTitle('Simple')
+    w.show()
+
+    sys.exit(app.exec_())
