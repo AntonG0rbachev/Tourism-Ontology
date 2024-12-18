ed64900e442636ce50386b9b11784c2e0e3a4660
DocLivsey <doclivsey@github.com>
2024-12-18 17:41:27 +0300

    feat: created class for my ontology

--- ontology/ontology.py
+++ ontology/ontology.py
@@ -13,6 +13,21 @@ class TourismOntology:
     def __init__(self):
         self.__ontology = get_tourism_ontology()
 
+    def get_ontology(self) -> Ontology:
+        return self.__ontology
+
+    def get_all_tours(self):
+        return tuple(self.__ontology.Поездки.instances())
+
+    def get_tour(self, tour: str = None):
+        return self.__ontology.Поездки(tour) if tour else self.get_all_tours()
+
+    def get_all_services(self):
+        return tuple(self.__ontology.Услуги.instances())
+
+    def get_service(self, service: str = None):
+        return self.__ontology.Услуги(service) if service else self.get_all_services()
+
 
 ontology = get_tourism_ontology()
 
@@ -23,4 +38,4 @@ print(list(ontology.object_properties()))
 
 print()
 for service in ontology.Услуги.instances():
-    print(ontology.Поездки(service).входит_в)
+    print(ontology.Услуги(service).входит_в)
