90e49f3771b266999de0909ffccd8fdeb55782d7
DocLivsey <doclivsey@github.com>
2024-12-18 17:25:52 +0300

    feat: made tours dialog window

---
+++ application/tours.py
@@ -0,0 +1,12 @@
+from PyQt5.QtWidgets import QDialog, QLabel, QVBoxLayout
+
+
+class ToursDialog(QDialog):
+    def __init__(self, result, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Доступные туры")
+
+        result_label = QLabel(f"{result}")
+        layout = QVBoxLayout()
+        layout.addWidget(result_label)
+        self.setLayout(layout)
