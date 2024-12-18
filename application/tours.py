69e18636948e0a5e361e8b45d5851a9db40b093c
DocLivsey <doclivsey@github.com>
2024-12-18 17:26:53 +0300

    feat: made services dialog window

--- application/tours.py
+++ application/tours.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import QDialog, QLabel, QVBoxLayout
 class ToursDialog(QDialog):
     def __init__(self, result, parent=None):
         super().__init__(parent)
-        self.setWindowTitle("Доступные туры")
+        self.setWindowTitle("Все доступные туры")
 
         result_label = QLabel(f"{result}")
         layout = QVBoxLayout()
